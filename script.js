6b8d5dd2c7bcb65d5c3d8df70790f5b0fc8b3f17
Luís Estevam <luisfestevam01@gmail.com>
2024-02-27 22:56:54 -0300

    Atualizações
    Melhorias na organização do HTML e do CSS, e implementação de menu mobile.

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //TROCAR RELOGIO
-const imgs = document.querySelectorAll(".cards img");
+function trocarRelogio() {
+  const imgs = document.querySelectorAll(".cards img");
+  if (imgs.length) {
+    imgs.forEach((img) => {
+      img.addEventListener("click", pegarTarget);
+    });
 
-imgs.forEach((img) => {
-  img.addEventListener("click", pegarTarget);
-});
-
-function pegarTarget(event) {
-  document.querySelector("#galeria img").outerHTML =
-    event.currentTarget.outerHTML;
+    function pegarTarget(event) {
+      document.querySelector(".relogio-grande img").outerHTML =
+        event.currentTarget.outerHTML;
+    }
+  }
 }
+trocarRelogio();
+
 //MENU MOBILE
+const btnMenuMobile = document.querySelector(".menuIcon");
+const btnMenuMobileImg = document.querySelector(".menuIcon img");
+const x = document.querySelector(".fecharMenu img");
+
+function ativarMenuMobile() {
+  const nav = document.querySelector(".menu-links");
+  nav.classList.toggle("ativo");
+}
+
+btnMenuMobile.addEventListener("click", ativarMenuMobile);
+
+//ANIMAÇÃO AO SCROLL
